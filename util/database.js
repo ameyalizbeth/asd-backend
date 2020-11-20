e4ef9f95e80ae53fa90dd135dbecfc88640a7c04
ameya <ameyalizbeth@gmail.com>
2020-11-20 23:11:00 +0530

    heroku

--- util/database.js
+++ util/database.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('asd','root','ameya',{dialect:'mysql',host:'localhost'});
+const sequelize = new Sequelize('heroku_cbf970c362cb60e','bb38a1704471bf','8518b9e7',{dialect:'mysql',host:'us-cdbr-east-02.cleardb.com'});
 
 
 module.exports=sequelize;
