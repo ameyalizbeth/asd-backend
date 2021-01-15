d7d63a217c186ffafea2a82925f2e906e6b6c605
ameya <44024565+ameyalizbeth@users.noreply.github.com>
2021-01-15 20:37:33 +0530

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,4 +1,4 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('heroku_a58b156ac875b24','ba5e1a571c373b','994e4e10',{dialect:'mysql',host:'us-cdbr-east-03.cleardb.com'});
+const sequelize = new Sequelize('heroku_a39ab545e43a14c','b5f224b260350d','73fc952a',{dialect:'mysql',host:'us-cdbr-east-03.cleardb.com'});
 
 module.exports=sequelize;
